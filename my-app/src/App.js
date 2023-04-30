9326a788a4a3f472504e7e4e5a84520d97ea7d95
Ajay Shankar <ajayshankar65@yahoo.com>
2023-04-30 16:22:55 -0400

    added Home and Data wrappers

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import Data from './Data';
+import Navbar from './Navbar';
+import About from './About';
+import Projects from './Projects';
+import Home from './Home';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Data />
+      <Navbar />
+      <HomeWrapper />
+      <Routes>
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+      <DataWrapper />
     </div>
   );
 }
 
+function HomeWrapper(){
+  const location = useLocation();
+  if(location.pathname === '/'){
+    return <Home />;
+  }
+  return null;
+}
+
+function DataWrapper(){
+  const location = useLocation();
+  if(location.pathname === '/projects'){
+    return <Data />;
+  }
+  return null;
+}
+
 export default App;
+
