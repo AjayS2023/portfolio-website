4f66e8a6f7921c649ff3db4be3b558702e41047b
Ajay Shankar <ajayshankar65@yahoo.com>
2023-05-07 16:23:22 -0400

    got home page set up

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -2,9 +2,14 @@ import React from "react";
 
 function Home() {
     return (
-        <div>
-            <h1>Welcome to my website</h1>
+        <div class="home-page">
+            <h3> Aspiring Fullstack Developer</h3>
+            <p>
+                Hi. I'm Ajay, a computer science enthusiast with passion for
+                building fullstack applications. 
+            </p>
         </div>
+
     );
 }
 
