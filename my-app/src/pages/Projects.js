cd69578e3e380eb469e59b2b2daf9063154640ca
Ajay Shankar <ajayshankar65@yahoo.com>
2023-05-07 16:23:52 -0400

    got Projects page set up with cards

--- my-app/src/pages/Projects.js
+++ my-app/src/pages/Projects.js
@@ -2,7 +2,141 @@ import React from "react";
 
 function Projects() {
     return (
-        <h2>Projects</h2>
+        <div class="card-container">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Card description goes here.</p>
+                    <button class="btn btn-primary">Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Card description goes here.</p>
+                    <button class="btn btn-primary">Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Card description goes here.</p>
+                    <button class="btn btn-primary">Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Card Title</h3>
+                    <p class="card-text"> Card description goes here.</p>
+                    <button>Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Card Title</h3>
+                    <p class="card-text"> Card description goes here.</p>
+                    <button>Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Card Title</h3>
+                    <p class="card-text"> Card description goes here.</p>
+                    <button>Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Card Title</h3>
+                    <p class="card-text"> Card description goes here.</p>
+                    <button>Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Card Title</h3>
+                    <p class="card-text"> Card description goes here.</p>
+                    <button>Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Card Title</h3>
+                    <p class="card-text"> Card description goes here.</p>
+                    <button>Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Card Title</h3>
+                    <p class="card-text"> Card description goes here.</p>
+                    <button>Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Card Title</h3>
+                    <p class="card-text"> Card description goes here.</p>
+                    <button>Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Card Title</h3>
+                    <p class="card-text"> Card description goes here.</p>
+                    <button>Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Card Title</h3>
+                    <p class="card-text"> Card description goes here.</p>
+                    <button>Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Card Title</h3>
+                    <p class="card-text"> Card description goes here.</p>
+                    <button>Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Card Title</h3>
+                    <p class="card-text"> Card description goes here.</p>
+                    <button>Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Card Title</h3>
+                    <p class="card-text"> Card description goes here.</p>
+                    <button>Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Card Title</h3>
+                    <p class="card-text"> Card description goes here.</p>
+                    <button>Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Card Title</h3>
+                    <p class="card-text"> Card description goes here.</p>
+                    <button>Button Text</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Card Title</h3>
+                    <p class="card-text"> Card description goes here.</p>
+                    <button>Button Text</button>
+                </div>
+            </div>
+        </div>
     );
 }
 
