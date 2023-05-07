b7453dd057bf4a42b3341ef94677591ebdace54d
Ajay Shankar <ajayshankar65@yahoo.com>
2023-05-07 16:22:05 -0400

    added about page, introduces me to viewers

---
+++ my-app/src/pages/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ProfessionalImage from '../images/ProfessionalImage.jfif';
+
+function About() {
+    return (
+        <div class="about-container">
+
+            <div class="content">
+                <h1 className="header">About Ajay Shankar</h1>
+                <img src={ProfessionalImage} />
+                <p>
+                    I am a Computer Science student at the University of North Carolina
+                    at Charlotte. My main interests lie in fullstack development. Building modern websites
+                    require nuance as well as understanding of the underlying systems, which is
+                    why I strive to keep up with the constantly evolving technologies.
+                </p>
+
+                <p>
+                    As an aspiring fullstack developer, I have experience with using HTML, CSS, and JavaScript
+                    to create powerful, responsive websites.
+                </p>
+                <p>
+                    Some of the technologies, frameworks, and stacks I have used are:
+                </p>
+                <ul>
+                    <li>Flask + Bootstrap + Postgres</li>
+                    <li>The MERN Stack (MongoDB, Express, React, Node)</li>
+                    <li>Flutter and Firebase</li>
+                    <li>Java</li>
+                    <li>HTML, CSS, JavaScript</li>
+                </ul>
+
+            </div>
+        </div>
+    );
+}
+
+export default About;
