0a6b3ab6bb6b8aca4510f32d7945de66d89b124f
Ajay Shankar <ajayshankar65@yahoo.com>
2023-05-07 16:20:52 -0400

    finalized navbar and footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,22 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
 import React from 'react';
 
-function Navbar() {
+function Footer() {
     return (
-
-        <nav className="navbar fixed-bottom navbar-expand-lg navbar-dark bg-primary">
-            <div className="container-fluid">
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <Link to="/" className='nav-link'>LinkedIn</Link>
-                        <Link to="/about" className="nav-link">About</Link>
-                        <Link to="/projects" className="nav-link">Projects</Link>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <div className="footer-container">
+            <p>&copy; Ajay Shankar. All Rights Reserved.</p>
+        </div>
     );
 }
 
-export default Navbar;
+export default Footer;
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,25 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 import React from 'react';
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <div className="container-fluid">
-                <a className="navbar-brand">My Website</a>
-                {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button> */}
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <Link to="/" className='nav-link'>Home</Link>
-                        <Link to="/projects" className="nav-link">Projects</Link>
-                        <Link to="/skills" className='nav-link'> Skills </Link>
-                        <Link to="/contact" className='nav-link'>Contact</Link>                        
-                    </ul>
-                </div>
-            </div>
+        <nav>
+            <ul className="navbar">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about"> About </Link></li>
+                <li><Link to="/contact"> Contact </Link></li>
+                <li><Link to="/projects"> Projects </Link></li>
+            </ul>
+
         </nav>
+
     );
 }
 
