c8536748b51c74a389c29c39dcfd2252edd9d9c3
Ajay Shankar <ajayshankar65@yahoo.com>
2023-05-14 23:18:58 -0400

    directly provides project rendering

--- my-server/server.js
+++ my-server/server.js
@@ -1,9 +1,42 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const { getAllProjects } = require('./controllers/projectController');
+const projectRoutes = require('./routes/projectRoutes');
+const Project = require('./models/projectModel');
 
 const app = express();
 
 app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    next();
+});
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to the database');
+        app.listen(process.env.PORT, () => {
+            console.log('Server is listening on port ' + process.env.PORT);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+// app.get('/api/projects', projectRoutes);
+app.get('/api/projects', async (req, res) => {
+    try {
+      const projects = await Project.find();
+      res.json(projects);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
+  
 
 app.get('/api/data', (req, res) => {
     const data = {
@@ -11,10 +44,4 @@ app.get('/api/data', (req, res) => {
         description: 'This is data from the server'
     };
     res.json(data);
-});
-
-const port = process.env.PORT || 4000;
-
-app.listen(port, ()=>{
-    console.log('Server is listening on port ' + port);
 });
