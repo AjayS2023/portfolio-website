26dd57e7edab00989f90cfdb03b5d609a01d15a7
Ajay Shankar <ajayshankar65@yahoo.com>
2023-05-14 23:15:53 -0400

    add project model for rendering

---
+++ my-server/models/projectModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+mongoose.set('strictQuery', true);
+
+const projectSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    technologies: {
+        type: [String],
+        required: true
+    },
+    githubLink: {
+        type: String,
+        required: true
+    },
+    deployLink: {
+        type: String,
+        required: true
+    }
+});
+
+const Project = mongoose.model('Project', projectSchema);
+module.exports = Project;
