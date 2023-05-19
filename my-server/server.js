d8eec6fb00630f405921740d61b962abdbd1205e
Ajay Shankar <ajayshankar65@yahoo.com>
2023-05-18 23:54:07 -0400

    refactors app to include projectRoutes

--- my-server/server.js
+++ my-server/server.js
@@ -2,9 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const { getAllProjects } = require('./controllers/projectController');
 const projectRoutes = require('./routes/projectRoutes');
-const Project = require('./models/projectModel');
+
 
 const app = express();
 
@@ -15,6 +14,8 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use('/api/projects', projectRoutes);
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('Connected to the database');
@@ -24,24 +25,4 @@ mongoose.connect(process.env.MONGO_URI)
     })
     .catch((err) => {
         console.log(err);
-    })
-
-// app.get('/api/projects', projectRoutes);
-app.get('/api/projects', async (req, res) => {
-    try {
-      const projects = await Project.find();
-      res.json(projects);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
-    }
-  });
-  
-
-app.get('/api/data', (req, res) => {
-    const data = {
-        title: 'Hello World',
-        description: 'This is data from the server'
-    };
-    res.json(data);
-});
+    });
