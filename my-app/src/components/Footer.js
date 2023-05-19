b043539a11c042a1a8485948e6249fa06d853c17
Ajay Shankar <ajayshankar65@yahoo.com>
2023-05-18 23:49:44 -0400

    add LinkedIn and Github icons to footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 function Footer() {
     return (
         <div className="footer-container">
             <p>&copy; Ajay Shankar. All Rights Reserved.</p>
+            <div className="social-icons">
+                <a href="https://www.linkedin.com/in/ajay-shankar2023/" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin size={43}/>
+                </a>
+                <a href="https://github.com/AjayS2023" target="_blank" rel="noopener noreferrer">
+                    <FaGithub size={43}/>
+                </a>
+            </div>
         </div>
     );
 }
