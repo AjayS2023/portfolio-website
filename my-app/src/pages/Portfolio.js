13020c365061db82ffd75ca4108afc6cb4005a88
Ajay Shankar <ajayshankar65@yahoo.com>
2023-05-21 11:22:38 -0400

    replace Projects page with Portfolio page

---
+++ my-app/src/pages/Portfolio.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+
+function Projects() {
+    const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    async function fetchProjects() {
+        const response = await fetch('http://localhost:4000/api/projects');
+        const data = await response.json();
+        setProjects(data);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        console.log('useEffect called');
+        fetchProjects();
+    }, []);
+
+    return (
+        <div className="project-container">
+            <h1 className="projects-header"> Full-Stack Development Portfolio </h1>
+            <div className="card-container">
+                {loading ? (
+                    <div>Loading...</div>
+                ) : (
+                    projects.map((project) => (
+                        <div className="card" key={project._id}>
+                            <div className="card-body">
+                                <h3 className="card-title">{project.title}</h3>
+                                <p className="card-text">{project.description}</p>
+                                <h5 className="card-text">Technologies: {project.techUsed}</h5>
+                                <button>
+                                    <a href={project.githubLink}>Visit Site</a>
+                                </button>
+                            </div>
+                        </div>
+                    ))
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default Projects;
