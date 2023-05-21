7560d7da9430a108593db70e8881bba63dba642f
Ajay Shankar <ajayshankar65@yahoo.com>
2023-05-21 11:23:56 -0400

    add Portfolio link to header

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -7,12 +7,9 @@ function Navbar() {
             <ul className="navbar">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about"> About </Link></li>
-                <li><Link to="/contact"> Contact </Link></li>
-                <li><Link to="/projects"> Projects </Link></li>
+                <li><Link to="/portfolio"> Portfolio </Link></li>
             </ul>
-
         </nav>
-
     );
 }
 
