6b2c05cd7727aa7091b9f2c9f5d7dbc799a64d5a
Ajay Shankar <ajayshankar65@yahoo.com>
2023-05-21 11:24:27 -0400

    add more detail to content

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -7,7 +7,7 @@ function About() {
 
             <div className="content">
                 <h1 className="header">About Ajay Shankar</h1>
-                <img src={ProfessionalImage} />
+                <img src={ProfessionalImage} alt="Ajay Shankar"/>
                 <p>
                     I am a Computer Science student at the University of North Carolina
                     at Charlotte. My main interests lie in fullstack development. Building modern websites
@@ -16,11 +16,8 @@ function About() {
                 </p>
 
                 <p>
-                    As an aspiring fullstack developer, I have experience with using HTML, CSS, and JavaScript
-                    to create powerful, responsive websites.
-                </p>
-                <p>
-                    Some of the technologies, frameworks, and stacks I have used are:
+                    As an aspiring fullstack developer, I have experience with using the stack
+                    to create powerful, responsive websites. Some of the technologies, frameworks, and stacks I have used are:
                 </p>
                 <ul>
                     <li>Flask + Bootstrap + Postgres</li>
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -3,11 +3,19 @@ import React from "react";
 function Home() {
     return (
         <div className="home-page">
-            <h3> Aspiring Fullstack Developer</h3>
-            <p>
-                Hi. I'm Ajay, a computer science enthusiast with passion for
-                building fullstack applications. 
-            </p>
+            <div className="content">
+                <h1 className="header"> Aspiring Fullstack Developer</h1>
+                <p>
+                    Hi. I'm Ajay, a computer science enthusiast with passion for
+                    building fullstack applications. Welcome to my personal website. It has been created
+                    using the MERN stack - MongoDB, Express.js, React, and Node.js.
+                </p>
+                <p> This page showcases some of my projects<a href="/projects">here.</a>
+                    You can see all of my projects in<a href="https://github.com/AjayS2023">my Github repo.</a>
+                    If you would like to contact me, you can do so through<a href="https://www.linkedin.com/in/ajay-shankar2023/">LinkedIn.</a>
+                    Please enjoy my website!
+                </p>
+            </div>
         </div>
 
     );
