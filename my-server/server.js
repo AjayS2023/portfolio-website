77703b54e0f1cfadf9bef5fb94f8f66054966aba
Ajay Shankar <ajayshankar65@yahoo.com>
2023-05-28 14:34:23 -0400

    add path security

--- my-server/server.js
+++ my-server/server.js
@@ -1,17 +1,19 @@
-require('dotenv').config({path: __dirname + '/.env'});
+require('dotenv').config({ path: __dirname + '/.env' });
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const projectRoutes = require('./routes/projectRoutes');
+const path = require('path');
+
 
 
 const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) =>{
-    res.send('Hello. Welcome to the website.');
-});
+// app.get('/', (req, res) =>{
+//     res.send('Hello. Welcome to the website.');
+// });
 
 app.use((req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
@@ -23,6 +25,13 @@ app.use('/api/portfolio', projectRoutes);
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('Connected to the database');
+
+        app.use(express.static(path.join(__dirname, 'my-app', 'build')));
+
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname, 'my-app', 'build', 'index.html'));
+        });
+
         app.listen(process.env.PORT, () => {
             console.log('Server is listening on port ' + process.env.PORT);
         });
