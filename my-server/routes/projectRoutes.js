68c13bae3b5cf7072c1b73afa7c7b8b2114c8498
Ajay Shankar <ajayshankar65@yahoo.com>
2024-01-01 16:05:18 +0530

    Refactors full stack application by removing server and simplifying app

--- my-server/routes/projectRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const {getAllProjects} = require('../controllers/projectController');
-
-const router = express.Router();
-
-// get all projects
-router.get('/', getAllProjects);
-
-module.exports = router;
