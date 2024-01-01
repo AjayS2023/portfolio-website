4fbb593a69498a33fc239b6ed65a1d70e772afc0
Ajay Shankar <ajayshankar65@yahoo.com>
2024-01-01 16:13:35 +0530

    adds cosmetic spacing code changes

--- my-app/src/pages/Portfolio.js
+++ my-app/src/pages/Portfolio.js
@@ -6,51 +6,45 @@ function Projects() {
         <div className="project-container">
             <h1 className="projects-header"> Full-Stack Development Portfolio </h1>
             <div className="card-container">
-                {/* {loading ? (
-                    <div>Loading...</div>
-                ) : (
-                    projects.map((project) => ( */}
-                        <div className="card">
-                            <div className="card-body">
-                                <h3 className="card-title">Portfolio Website</h3>
-                                {/* <p className="card-text">{project.description}</p> */}
-                                <h5 className="card-text">Technologies: React, Node.js, Bootstrap</h5>
-                                <button>
-                                    <a href='https://github.com/AjayS2023/portfolio-website'>Visit Site</a>
-                                </button>
-                            </div>
-                        </div>
 
-                        <div className="card">
-                            <div className="card-body">
-                                <h3 className="card-title">Cricket Database</h3>
-                                <h5 className="card-text">Technologies: Java</h5>
-                                <button>
-                                    <a href="https://github.com/AjayS2023/portfolio-website">Visit Site</a>
-                                </button>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="card-body">
-                                <h3 className="card-title">UNC Charlotte Blog</h3>
-                                <h5 className="card-text">Technologies: Go, Gin</h5>
-                                <button>
-                                    <a href="https://github.com/AjayS2023/portfolio-website">Visit Site</a>
-                                </button>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="card-body">
-                                <h3 className="card-title">Star Wars Trading</h3>
-                                <h5 className="card-text">Technologies: MongoDB, Express.js, EJS, Node.js</h5>
-                                <button>
-                                    <a href="https://github.com/AjayS2023/portfolio-website">Visit Site</a>
-                                </button>
-                            </div>
-                        </div>
+                <div className="card">
+                    <div className="card-body">
+                        <h3 className="card-title">Portfolio Website</h3>
+                        <h5 className="card-text">Technologies: React, Node.js, Bootstrap</h5>
+                        <button>
+                            <a href='https://github.com/AjayS2023/portfolio-website'>Visit Site</a>
+                        </button>
+                    </div>
+                </div>
 
-                    {/* ))
-                )} */}
+                <div className="card">
+                    <div className="card-body">
+                        <h3 className="card-title">Cricket Database</h3>
+                        <h5 className="card-text">Technologies: Java</h5>
+                        <button>
+                            <a href="https://github.com/AjayS2023/portfolio-website">Visit Site</a>
+                        </button>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-body">
+                        <h3 className="card-title">UNC Charlotte Blog</h3>
+                        <h5 className="card-text">Technologies: Go, Gin</h5>
+                        <button>
+                            <a href="https://github.com/AjayS2023/portfolio-website">Visit Site</a>
+                        </button>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-body">
+                        <h3 className="card-title">Star Wars Trading</h3>
+                        <h5 className="card-text">Technologies: MongoDB, Express.js, EJS, Node.js</h5>
+                        <button>
+                            <a href="https://github.com/AjayS2023/portfolio-website">Visit Site</a>
+                        </button>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
